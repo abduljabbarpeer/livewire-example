2389517199a0a2b946fc4b3640f66deca938c27c
abduljabbarpeer <abduljabbarpeer@gmail.com>
2022-01-19 18:43:52 +0530

    initial livewire template - basic version

---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Some Data About Hello World, What's your name btw {{$name}}??;
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Livewire sample</title>
+        @livewireStyles
+    </head>
+    <body>
+        @livewire('hello-world')
+    
+        @livewireScripts
+    </body>
+</html>
